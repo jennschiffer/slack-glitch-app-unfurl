dda63f43b563e67b10c3f493d2e5fac767ef6115
System <none>
2017-03-09 15:56:04 +0000

    add norm links to handle non-project page unfurls

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Glitch's Slack App Unfurling App
-This project demonstrates how to build an app unfurling app using Slack's Events API and new chat unfurl endpoint.
+This project demonstrates how to build an app unfurling app using Slack's Events API and new chat unfurl endpoint, using Glitch project domains!
 
 
 ## Getting Started
 To get started you need to:
 - Set up your Slack App in Slack
 - Copy across the Slack API tokens into the `.env` file
+- the index html is generated in `tinyspeck.js` around line 194
 
 For more detailed setup instructions, see `setup.md`.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //
 // This implements most of the app code and implements code that sets up 
-// event handling for when the 'url_shared' event is activated.
+// event handling for when the 'link_shared' event is activated.
 //
 
 "use strict";
@@ -9,52 +9,54 @@ const slack = require('./tinyspeck.js');
 const axios = require('axios');
 const glitchEndpoint = 'https://api.gomix.com/projects/';
 
-// listen for a url to be shared in a message
-slack.on('url_shared', payload => {  
-  
-  // we're going to collect an array of objects of "unfurls" to send back
-  const unfurls = {};
-  
-  // we want to keep track of projectNames so we don't post duplicate unfurls
-  const projectNames = [];
-  
-  // we need to track promises to make sure we get all projects information
-  // from the glitch endpoints hit before sending to slack
-  const projectsPromises = [];
+// some link and style defaults
+const normLinks = require('./glitch-normlinks.js');
+const thumb_url = 'https://gomix.com/slack-icon.png';
+const color = '#ff00ff';
 
+// listen for a url to be shared in a message
+slack.on('link_shared', payload => {  
+  
+  // we are going to collect unfurl objects to send, project names to prevent duplicates,
+  // and promises to make sure we don't send messages to slack before glitch api calls 
+  // are responded to
+  const unfurls = {};
+  const projectNames = [];
+  const projectsPromises = [];
+  
   // for each even url in the payload, we're going to add an object
   // used by slack to message back the unfurled link to the `unfurls` object
-  payload.event.urls.forEach((url, i) => {
-    let validLink = false;
+  payload.event.links.forEach((link, i) => {
+    
+    console.log('link detected:', link);
+    
+    let projectLink = false;
+    let normLink = false;
     let projectName = '';
     
-    // these are a list of subdomains we use which should not be seen as valid project names
-    const invalidProjectNames = ['', 'gomix', 'glitch', 'status', 'support'];
-
-    // check if the domain is glitch.com, glitch.me, gomix.com, or gomix.me
-    if ( url.indexOf('gomix.me') > -1 || url.indexOf('glitch.me') > -1 ) {
+    if ( normLinks[link.url] ) {
       
+      // first we check if any of the urls match what we listed in `glitch-normlinks.js`
+      // because they are not project pages, so we do not want to unfurl project action links
+      
+      normLink = link.url;
+    }
+    else if ( link.domain === 'gomix.me' || link.domain === 'glitch.me' ) {
       // .me implies it's the project "show" page
-      const hostname = url.split('//')[1];
+      const hostname = link.url.split('//')[1];
       projectName = hostname.split('.')[0];
-      validLink = true;
       
-    } else if ( (url.indexOf('gomix.com') > -1 || url.indexOf('glitch.com') > -1) && url.indexOf('#!') > -1 ) {
-        
-      // .com plus a hashbang implies it's the project editor
-        const hostnameArray = url.split('/');
+    } else if ( (link.domain === 'gomix.com' || link.domain === 'glitch.com') && link.url.indexOf('#!/project') > -1 ) {
+        // .com plus a hashbang implies it's the project editor
+        const hostnameArray = link.url.split('/');
         projectName = hostnameArray[hostnameArray.length-1];
-        validLink = true;
+        projectLink = true;
       
-      } else {
-          // for some reason this link is in our list but not gomix/glitch.com/me
-          validLink = false;
-        }
-    
+      } 
+
     // now that we should have the project name, generate the attachment if
     // an attachment for this given project hasn't been generated yet
-    if ( validLink && !invalidProjectNames.includes(projectName) && !projectNames.includes(projectName) ) {
-      
+    if ( projectLink && !projectNames.includes(projectName) ) {
       // push project name to array so we prevent duplicates
       projectNames.push(projectName);
 
@@ -65,7 +67,6 @@ slack.on('url_shared', payload => {
       // any reference to axios in this file only
       projectsPromises.push(axios.get(glitchEndpoint + projectName)
         .then(function (response) {
-          
           // generate the items of the unfurl message and add that object to `unfurls`
           const title = projectName;
         
@@ -74,45 +75,52 @@ slack.on('url_shared', payload => {
           const remixLink = `<https://gomix.com/#!/remix/${projectName}|:microphone: Remix On Glitch>`;
           const text = `${response.data.description} \n\n ${showLink}    ${viewCode}    ${remixLink}`;
 
-          const thumb_url = 'https://gomix.com/slack-icon.png';
-          const fallback = url.substr(url.indexOf(':') + 1);
+          const fallback = link.url.substr(link.url.indexOf(':') + 1);
 
-          unfurls[url] = {
+          unfurls[link.url] = {
             title,
             text,
             fallback,
             thumb_url,
-            color: '#ff00ff',
+            color,
           };
         })
         .catch(function (error) {
           console.log(error);
         })
       );
-    } 
+    } else if ( normLink ) {
+      // non-project glitch links are handled here
+      unfurls[normLink] = normLinks[normLink];
+    }
   });
   
   // once all the promises have been completed, generate and send to slack the unfurls
   axios.all(projectsPromises)
   .then(axios.spread(function(acct, perms) {
     
-    // here we create the final message object to send to slack, which includes
-    // the unfurls object we created above and the channel and ts properties 
-    // we retrieved from the payload
-    const unfurlMessage = {
-      channel: payload.event.channel,
-      ts: payload.event.message_ts,
-      unfurls: JSON.stringify(unfurls)
-    }
+    if ( Object.getOwnPropertyNames(unfurls).length !== 0 ) {
+      
+      console.log('unfurls sent:', unfurls);
+      
+      // here we create the final message object to send to slack, which includes
+      // the unfurls object we created above and the channel and ts properties 
+      // we retrieved from the payload, if there are any unfurls to send
+      const unfurlMessage = {
+        channel: payload.event.channel,
+        ts: payload.event.message_ts,
+        unfurls: JSON.stringify(unfurls)
+      }
 
-    // finally, we send slack the unfurlMessage by calling the chat.unfurl endpoint
-    // with unfurlMessage as the argument. 
-    slack.send('chat.unfurl', unfurlMessage).then(res => { 
-      console.log("Response sent to unfurl", res.data);
-    }, reason => { 
-      // on failure
-      console.log("An error occurred when responding to unfurl: " + reason);
-    }); 
+      // finally, we send slack the unfurlMessage by calling the chat.unfurl endpoint
+      // with unfurlMessage as the argument. 
+      slack.send('chat.unfurl', unfurlMessage).then(res => { 
+        console.log("Response sent to unfurl", res.data);
+      }, reason => { 
+        // on failure
+        console.log("An error occurred when responding to unfurl: " + reason);
+      }); 
+    }
   }));
 }); 
 
